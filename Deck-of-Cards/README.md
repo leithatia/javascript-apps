c12e69ff90344b8ca936d35e3e837072a5157d87
Leith Atia <leith.atia@gmail.com>
2023-03-07 18:18:59 +0000

    Refactor code creating deck object with shuffleDeck and drawCard methods

--- Deck-of-Cards/README.md
+++ Deck-of-Cards/README.md
@@ -1,1 +1,1 @@
-Simple app working with playing cards
+Simple app using an object to create a deck of cards, shuffle the deck and draw a card
--- Deck-of-Cards/app.js
+++ Deck-of-Cards/app.js
@@ -1,13 +1,31 @@
-function makeDeck() {
-  const deck = [];
-  const suits = ["hearts", "diamonds", "spades", "clubs"];
-  const values = "2,3,4,5,6,7,8,9,10,J,Q,K,A";
-
-  for (let value of values.split(',')) {
-    for (let suit of suits) {
-      deck.push({value,
-                  suit});
+const makeDeck = () => {
+  return {
+    deck: [],
+    drawnCards: [],
+    suits: ["hearts", "diamonds", "spades", "clubs"],
+    values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
+    initialiseDeck() {
+      const {deck, suits, values} = this;
+      for (let suit of suits) {
+        for (let value of values.split(',')) {
+          deck.push({value, suit});
+        }
+      }
+    },
+    shuffleDeck() {
+      const {deck} = this;
+      for (let i = deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+      }
+    },
+    drawCard() {
+      const drawnCard = this.deck.pop();
+      this.drawnCards.push(drawnCard)
+      return drawnCard;
     }
   }
-  return deck;
 }
+
+const deck1 = makeDeck();
+deck1.initialiseDeck();
