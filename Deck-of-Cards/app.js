1cbab4828bdda44f87bcd892667ecd631f9ddfed
Leith Atia <leith.atia@gmail.com>
2023-03-07 14:40:32 +0000

    Create makeDeck() function

--- Deck-of-Cards/app.js
+++ Deck-of-Cards/app.js
@@ -1,1 +1,13 @@
-console.log("Testing");
+function makeDeck() {
+  const deck = [];
+  const suits = ["hearts", "diamonds", "spades", "clubs"];
+  const values = "2,3,4,5,6,7,8,9,10,J,Q,K,A";
+
+  for (let value of values.split(',')) {
+    for (let suit of suits) {
+      deck.push({value,
+                  suit});
+    }
+  }
+  return deck;
+}
