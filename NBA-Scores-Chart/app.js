59ac7b88e9768d1b3b88a6f6363ef416dd1fde94
Leith Atia <leith.atia@gmail.com>
2023-03-13 10:04:15 +0000

    Initial push for NBA Charts

---
+++ NBA-Scores-Chart/app.js
@@ -0,0 +1,97 @@
+ const warriorsGames = [{
+    awayTeam: {
+      team: 'Golden State',
+      points: 119,
+      isWinner: true
+    },
+    homeTeam: {
+      team: 'Houston',
+      points: 106,
+      isWinner: false
+    }
+  },
+  {
+    awayTeam: {
+      team: 'Golden State',
+      points: 105,
+      isWinner: false
+    },
+    homeTeam: {
+      team: 'Houston',
+      points: 127,
+      isWinner: true
+    }
+  },
+  {
+    homeTeam: {
+      team: 'Golden State',
+      points: 126,
+      isWinner: true
+    },
+    awayTeam: {
+      team: 'Houston',
+      points: 85,
+      isWinner: false
+    }
+  },
+  {
+    homeTeam: {
+      team: 'Golden State',
+      points: 92,
+      isWinner: false
+    },
+    awayTeam: {
+      team: 'Houston',
+      points: 95,
+      isWinner: true
+    }
+  },
+  {
+    awayTeam: {
+      team: 'Golden State',
+      points: 94,
+      isWinner: false
+    },
+    homeTeam: {
+      team: 'Houston',
+      points: 98,
+      isWinner: true
+    }
+  },
+  {
+    homeTeam: {
+      team: 'Golden State',
+      points: 115,
+      isWinner: true
+    },
+    awayTeam: {
+      team: 'Houston',
+      points: 86,
+      isWinner: false
+    }
+  },
+  {
+    awayTeam: {
+      team: 'Golden State',
+      points: 101,
+      isWinner: true
+    },
+    homeTeam: {
+      team: 'Houston',
+      points: 92,
+      isWinner: false
+    }
+  }
+]
+
+const ulParent = document.createElement("ul");
+
+warriorsGames.forEach(game => {
+  const {awayTeam, homeTeam} = game;
+  const li = document.createElement("li");
+
+  li.innerText = `${awayTeam.team} @ ${homeTeam.team}`;
+  ulParent.appendChild(li);
+});
+
+document.body.append(ulParent);
